db66762ebd9f006b80c3ced2faf3c01a428f3212
rodrfentanes21 <rfentanes@sga.pucminas.br>
2025-02-21 18:51:28 +0100

    config: eslint/prettier project configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,26 @@
-import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
+import './styles/globals.css';
+import { JSX } from 'react';
 
 const montserrat = Montserrat({
-  subsets: ["latin"],
-  variable: "--font-montserrat",
+    subsets: ['latin'],
+    variable: '--font-montserrat',
 });
 
 export const metadata: Metadata = {
-  title: "My App",
-  description: "My App Description",
+    title: 'My App',
+    description: 'My App Description',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={`${montserrat.variable} font-montserrat`}>
-        {children}
-      </body>
-    </html>
-  );
+    children: React.ReactNode;
+}>): JSX.Element {
+    return (
+        <html lang="en">
+            <body className={`${montserrat.variable} font-montserrat`}>{children}</body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl">Hello World! GGG</h1>
-    </div>
-  );
+import { JSX } from 'react';
+
+export default function Home(): JSX.Element {
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl">Hello World!</h1>
+        </div>
+    );
 }
