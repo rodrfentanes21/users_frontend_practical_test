0174d8730b617f637ef34585e78deaa8a511f55b
rodrfentanes21 <rfentanes@sga.pucminas.br>
2025-02-24 19:34:42 +0100

    feat: improve search bar responsivity

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -14,10 +14,11 @@ export function SearchBar(): JSX.Element {
                 type="text"
                 value={searchQuery}
                 onChange={handleSearch}
-                placeholder="Search users by name, email, username or city..."
-                className="w-full px-4 py-2 rounded-lg border-2 border-indigo-500/20 focus:border-indigo-600 focus:outline-none bg-white dark:bg-black text-gray-800 dark:text-gray-200 placeholder-gray-500 dark:placeholder-gray-400 transition-all duration-300"
-                aria-label="Search users"
+                className="w-full px-4 sm:pl-[11.5rem] py-2 rounded-lg border-2 border-indigo-500/20 focus:border-indigo-600 focus:outline-none bg-white dark:bg-black text-gray-800 dark:text-gray-200 placeholder-gray-500 dark:placeholder-gray-400 transition-all duration-300"
+                aria-label="Search users by name, email, username or city"
+                placeholder="Search users..."
             />
+            <span className="sr-only">Search users by name, email, username or city</span>
             <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
                 <svg
                     className="w-5 h-5 text-indigo-600 dark:text-indigo-400"
@@ -25,6 +26,7 @@ export function SearchBar(): JSX.Element {
                     stroke="currentColor"
                     viewBox="0 0 24 24"
                     xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
                 >
                     <path
                         strokeLinecap="round"
